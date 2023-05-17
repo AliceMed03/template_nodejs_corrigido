b72c1576b0c74b5d4cf46c3625d1b5637759eb5a
AliceMed03 <alicemedeirosds@gmail.com>
2023-05-17 19:29:11 -0300

    incluindo arquivos

---
+++ data/migrations/20230517221723_create_editoras.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+    return knex.schema.createTable('editoras', (table)=> {
+      table.increments();
+      table.string("nome",50).notNullable();
+      table.string("cidade",20).notNullable();
+      table.string("estado",20).notNullable();
+      table.string("telefone",15).notNullable();
+      table.string("rua",50).notNullable();
+      table.integer("CEP",8).notNullable();
+    });
+  };
+  exports.down = function(knex) {
+    return knex.schema.dropTable("editoras");
+  };
+  
